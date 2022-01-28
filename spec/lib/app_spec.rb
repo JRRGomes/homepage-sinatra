cf7fd42bbb8418b5e851c3879d37f6afd1fd904e
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-01-28 17:26:07 -0300

    feat: Tests created for every routes response and response status

--- spec/lib/app_spec.rb
+++ spec/lib/app_spec.rb
@@ -4,25 +4,40 @@ require './app.rb'
 require 'rspec'
 require 'rack/test'
 
-RSpec.describe 'The HelloWorld App' do
+RSpec.describe 'The Homepage App' do
   include Rack::Test::Methods
 
   def app
     Sinatra::Application
   end
 
-  it "test route to index.erb" do
+  it "should test route to index.erb" do
     get '/'
     expect(last_response).to be_ok
   end
 
-  it "test route to projects.erb" do
+  it "should test route to projects.erb" do
     get '/projects.erb'
     expect(last_response).to be_ok
   end
 
-  it "test route to education.erb" do
+  it "should test route to education.erb" do
     get '/education.erb'
     expect(last_response).to be_ok
+  end
+
+  it "should returns status of index.erb to be 200" do
+    get '/'
+    expect(last_response.status).to eq 200
+  end
+
+  it "should returns status of projects.erb to be 200" do
+    get '/projects.erb'
+    expect(last_response.status).to eq 200
+  end
+
+  it "should returns status of education.erb to be 200" do
+    get '/education.erb'
+    expect(last_response.status).to eq 200
   end
 end
